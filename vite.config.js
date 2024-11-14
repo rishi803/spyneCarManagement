b904af198f43f23ad52ce04174140ffdd749bf92
rishi singh <85188638+rishi803@users.noreply.github.com>
2024-11-15 01:50:01 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  },
+  
   plugins: [react()],
 })
